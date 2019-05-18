cf1f270a353eba817d73ca2d77066c237948e7aa
Bane <fengxiaotx@163.com>
2019-05-18 20:32:46 +0800

    add giraffe ui

--- src/giraffe.js
+++ src/giraffe.js
@@ -1,4 +1,9 @@
 import Quill from "quill";
+
+const icons = Quill.import("ui/icons");
+let Size = Quill.import('attributors/style/size');
+Size.whitelist = ['9pt', '10pt', '11pt', '12pt','14pt','16pt','18pt','22pt','24pt','30pt','36pt'];
+Quill.register(Size, true);
 class Giraffe extends Quill{
 
     constructor(container, options) {
