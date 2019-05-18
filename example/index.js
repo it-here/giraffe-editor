39531ca32cbf01baa5379bbe31d719e0535d739f
Bane <fengxiaotx@163.com>
2019-05-18 14:51:44 +0800

    add giraffe ui

--- example/index.js
+++ example/index.js
@@ -5,6 +5,22 @@
  * Date: 2019-05-18
  * Time: 08:49
  */
+import "../src/giraffe.less";
+ import GiraffeEditor from "../src/giraffe";
+// import Quill from "quill";
+import "quill/dist/quill.snow.css"
 $(function () {
 
+    var quill = new GiraffeEditor('#editor', {
+        modules: { toolbar: '#toolbar' },
+        theme: 'snow'
+    });
+    let title = document.createElement('div');
+    title.className = 'ql-title';
+    let titleBox = document.createElement('div');
+    titleBox.className = 'ql-title-box';
+    let titleInput = document.createElement('input');
+    titleBox.append(titleInput);
+    title.append(titleBox);
+    quill.addContainer(title,document.querySelector(".ql-editor"));
 });
--- src/giraffe.js
+++ src/giraffe.js
@@ -1,4 +1,8 @@
-class Giraffe {
+import Quill from "quill";
+class Giraffe extends Quill{
 
+    constructor(container, options) {
+        super(container,options)
+    }
 }
 export default Giraffe
