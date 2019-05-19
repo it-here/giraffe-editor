1fd99242196735da940b3e745d9f3a971c250f07
Bane <fengxiaotx@163.com>
2019-05-19 16:24:43 +0800

    add giraffe ui

--- src/quill/quill.js
+++ src/quill/quill.js
@@ -7,15 +7,18 @@
  */
 import Quill from "quill";
 
-let Size = Quill.import('attributors/style/size');
-Size.whitelist = ['9pt', '10pt', '11pt', '12pt','14pt','16pt','18pt','22pt','24pt','30pt','36pt'];
+let AlignStyle = Quill.import('attributors/style/align');
+
+let SizeStyle = Quill.import('attributors/style/size');
+SizeStyle.whitelist = ['9pt', '10pt', '11pt', '12pt','14pt','16pt','18pt','22pt','24pt','30pt','36pt'];
 
 
 const Icons = Object.assign(Quill.import("ui/icons"),require("../ui/icons"));
 
 
 Quill.register({
-    'formats/size': Size,
+    'formats/align': AlignStyle,
+    'formats/size': SizeStyle,
     'ui/icons': Icons,
 }, true);
 
