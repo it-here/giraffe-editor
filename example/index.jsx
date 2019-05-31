72d3360f13c347fe89e234e436a9994abd719d3d
baneshi <fengxiaotx@163.com>
2019-05-31 23:19:03 +0800

    init

--- example/index.jsx
+++ example/index.jsx
@@ -7,35 +7,9 @@
  */
 import "../src/assets/giraffe-editor.less";
  import Giraffe from "../src/Giraffe";
-// import Quill from "quill";
-// import "quill/dist/quill.snow.css"
 import React from "react";
 import * as ReactDom from "react-dom";
-$(function () {
-
-    // var quill = new GiraffeEditor('#editor', {
-    //     modules: {
-    //         toolbar: '#toolbar' ,
-    //         imageResize:{
-    //             modules: [ 'Resize', 'DisplaySize', 'Toolbar' ]
-    //         },
-    //         imageDrop: true
-    //     },
-    //     theme: 'snow'
-    // });
-    // let title = document.createElement('div');
-    // title.className = 'ql-title';
-    // let titleBox = document.createElement('div');
-    // titleBox.className = 'ql-title-box';
-    // let titleInput = document.createElement('input');
-    // titleBox.append(titleInput);
-    // title.append(titleBox);
-    // quill.addContainer(title,document.querySelector(".ql-editor"));
-
-    ReactDom.render(
-        <Giraffe/>,
-        document.getElementById("edit-demo")
-    );
-
-
-});
+ReactDom.render(
+    <Giraffe/>,
+    document.getElementById("edit-demo")
+);
